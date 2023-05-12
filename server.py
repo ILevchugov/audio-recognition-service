6c3d9001054c727f13325f1f0195828e98e21a4e
ILevchugov <i.levchugov@gmail.com>
2023-05-12 19:09:26 +0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,47 @@
+import speech_recognition as sr
+from pydub import AudioSegment
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+
+        if file.filename == "":
+            return redirect(request.url)
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data)
+
+    return render_template('index.html', transcript=transcript)
+
+
+@app.route('/recognition/audio/mp3', methods=['POST'])
+def recognise_mp3():
+    if request.method == 'POST':
+        f = request.files['file']
+
+    x = AudioSegment.from_file(f.stream)
+    file = x.export(format="wav")
+
+    mp3 = sr.AudioFile(file)
+
+    r = sr.Recognizer()
+
+    with mp3 as source:
+        audio = r.record(source)
+    rec = r.recognize_google(audio)
+    return rec
